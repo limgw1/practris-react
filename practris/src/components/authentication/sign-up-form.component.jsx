cff067247105549bc9146208d559f5603475cc43
limgw <iamapokemonmaster@gmail.com>
2023-01-21 09:05:26 +0800

    Added some more functionality for login

--- practris/src/components/authentication/sign-up-form.component.jsx
+++ practris/src/components/authentication/sign-up-form.component.jsx
@@ -1,6 +1,56 @@
+import {useState} from 'react'
+import { createAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils'
+
+//Instead of using 4 different useStates, can use this alternative
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+}
+
 const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields)
+  const {displayName, email, password, confirmPassword} = formFields
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    //Check if passwords match
+    if(password !== confirmPassword) {
+      alert("Passwords do not match")
+      return
+    }
+    //actually try to authenticate
+    try{
+      const response = await createAuthUserWithEmailAndPassword(email, password)
+      console.log("Creating user")
+      console.log(response)
+    }catch(error){
+      console.log("User creation encountered an error.", error)
+    }
+  }
+
+  const handleChange = async(event) => {
+    //get name and value from event.target
+    const {name,value} = event.target
+    setFormFields({...formFields, [name]: value})
+  }
+
   return(
-    <div>Sign Up Form</div>
+    <div>
+      <h1>Sign up with your email and password</h1>
+      <form onSubmit={handleSubmit}> {/* onSubmit is built into react */}
+        <label>Display name</label>
+        <input type="text" required onChange={handleChange} name="displayName" value={displayName}/>
+        <label>Email</label>
+        <input type="email" required onChange={handleChange} name="email" value={email}/>
+        <label>Password</label>
+        <input type="password" required onChange={handleChange} name="password" value={password}/>
+        <label>Confirm Password</label>
+        <input type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
+        <button type="submit">Submit</button> {/*onSubmit will read any button with onSubmit type*/}
+      </form>
+    </div>
   )
 }
 
---
+++ practris/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,49 @@
+import {initializeApp} from 'firebase/app'
+import {getAuth, signInWithPopup, createUserWithEmailAndPassword} from 'firebase/auth'
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVZNbkc5aFcaXAEkDUqvphSVDXLXG5LmE",
+  authDomain: "practris-f835f.firebaseapp.com",
+  projectId: "practris-f835f",
+  storageBucket: "practris-f835f.appspot.com",
+  messagingSenderId: "469631587360",
+  appId: "1:469631587360:web:ca087937b8651fe6101845",
+  measurementId: "G-G2YCDM3BZ1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth() //Instantiate Firebase's authentication instance
+export const db = getFirestore() //Instantiate Firebase's database instance
+
+
+// Create user document (basically creating a new account)
+export const createUserDocumentFromAuth = async (userAuth) => {
+  //If i dont get userAuth. return nothing. this is to protect frontend from firebase changes
+  if (!userAuth) return
+  const userDocRef = doc(db, 'users', userAuth.uid) //userDocRef is an object that is the reference to a certain point in Firestore
+  const userSnapshot = await getDoc(userDocRef) //getDoc returns the user data that userDocRef is pointing to
+
+  if(!userSnapshot.exists()) { //returns true if there is data at that reference.
+    // Get data to create new user document (account) with
+    const { displayName, email} = userAuth; //destructure displayName and email from userAuth
+    const createdAt = new Date()
+
+    try{
+      await setDoc(userDocRef, {displayName, email, createdAt})
+    }catch (error){
+      console.log('error creating the user', error.message)
+    }
+  }
+  //if user exists
+  return userDocRef
+}
+
+export const createAuthUserWithEmailAndPassword = async(email, password) => {
+  //If i dont get email or password. return nothing. this is to protect frontend from firebase changes
+  if(!email || !password) return
+  return await createUserWithEmailAndPassword(auth, email, password)
+}
